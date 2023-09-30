82583eeed4c49b201a3293d051fa4f71021bed49
Bryan Noel Salindeho <bryannoelsalindeho23@gmail.com>
2023-09-30 22:22:15 +0800

    tab headers

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
 
 function App() {
+  const [activeTab, setActiveTab] = useState("Analysis");
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className = "header">
+        <div className = {activeTab==="Analysis"? "tab-active" : "tab"} onClick={()=>setActiveTab("Analysis")}>Analysis</div>
+        <div className = {activeTab==="Berth"? "tab-active" : "tab"} onClick={()=>setActiveTab("Berth")}>Berth</div>
+        <div className = {activeTab==="Equipment"? "tab-active" : "tab"} onClick={()=>setActiveTab("Equipment")}>Equipment</div>
+      </div>
+      <div className = "content">
+      </div>
     </div>
   );
 }
